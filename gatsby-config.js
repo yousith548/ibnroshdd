46a548f21c6e22a032c9a8ee83f392a7c88194de
yousith548 <65802098+yousith548@users.noreply.github.com>
2020-06-20 00:08:24 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,19 +20,19 @@ exports.onPreBootstrap = ({ store, reporter }) => {
 module.exports = {
   siteMetadata: {
     title: 'Mosquée Averroès',
-    description: 'La Grande Mosquée Averroès de Montpellier',
+    description: 'La Grande Mosquée Averroès de Montpellier مسجد ابن رشد بموبيليي',
     coverImage: 'img/blog-cover.jpg',
     logo: 'img/ghost-logo.png',
     lang: 'fr', 
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
-    facebook: 'https://www.facebook.com/ghost',
-    twitter: 'https://twitter.com/tryghost',
+    siteUrl: 'https://www.acafibnrochd.com', // full path to blog - no ending slash
+    facebook: 'https://www.facebook.com/mosqueeibnrochd/',
+    twitter: 'https://twitter.com/mosqueeibnrochd',
     showSubscribe: false, // subscribe button in site nav and home page
     mailchimpAction: '', // 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
     mailchimpName: '', // 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
     mailchimpEmailFieldName: '', // 'MERGE0',
     googleSiteVerification: '', // 'GoogleCode',
-    footer: 'is based on Gatsby Casper',
+    footer: '',
     postsPerPage: 6,
   },
   mapping: {
